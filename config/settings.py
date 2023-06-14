9612eef9e5e807f851fbaaef5f3b3a3464545701
Mateus Raulino <49491707+raulinrivs@users.noreply.github.com>
2023-06-14 16:21:26 -0300

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -13,6 +13,7 @@ from pathlib import Path
 import os
 import sys
 from celery.schedules import crontab
+import dj_database_url
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
